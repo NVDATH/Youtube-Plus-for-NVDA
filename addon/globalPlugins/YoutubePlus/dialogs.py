c9321681cce4ba957912301df784ff3c9068c3d2
NVDA in Thai <nvdainth@gmail.com>
2026-03-19 06:45:50 +0700

    release again

--- addon/globalPlugins/YoutubePlus/dialogs.py
+++ addon/globalPlugins/YoutubePlus/dialogs.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 # dialogs.py for YoutubePlus NVDA Addon
+# Copyright (C) 2025
+# This file is covered by the GNU General Public License.
+# You can read the licence by clicking Help->Licence in the NVDA menu
+# or by visiting http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# Shortcut: Windows+y
 
 import wx
 import wx.lib.mixins.listctrl as listmix
--- addon/globalPlugins/YoutubePlus/errors.py
+++ addon/globalPlugins/YoutubePlus/errors.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 # errors.py for Youtube Plus NVDA Addon
+# Copyright (C) 2025
+# This file is covered by the GNU General Public License.
+# You can read the licence by clicking Help->Licence in the NVDA menu
+# or by visiting http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# Shortcut: Windows+y
 
 class HandledError(Exception):
     """
